41d51f183b0b204f1fa84ccb210e2ca47c0c5bf3
ludogwada <sinquet.ludovic@gmail.com>
2022-04-22 10:38:34 -0400

    test

--- src/__tests__/error.js
+++ src/__tests__/error.js
@@ -3,15 +3,15 @@ import { MemoryRouter } from 'react-router-dom';
 import Error from '../pages/error';
 
 describe('Given I am a visitor', () => {
-    describe('When I am on a Error page', () => {
-        test('Then, there should be on the page', () => {
-            render(
-                <MemoryRouter>
-                    <Error />
-                </MemoryRouter>
-            );
-            const errorText = screen.getByText('404');
-            expect(errorText.className).toBe('error__text');
-        });
-    });
+	describe('When I am on a Error page', () => {
+		test('Then, there should be on the page', () => {
+			render(
+				<MemoryRouter>
+					<Error />
+				</MemoryRouter>
+			);
+			const errorText = screen.getByText('404');
+			expect(errorText.className).toBe('error__text');
+		});
+	});
 });
--- src/__tests__/header.js
+++ src/__tests__/header.js
@@ -3,15 +3,15 @@ import { MemoryRouter } from 'react-router-dom';
 import Header from '../components/Header/header';
 
 describe('Given I am a visitor', () => {
-    describe('when I am on a Home page', () => {
-        test('Then,there should be on the page', () => {
-            render(
-                <MemoryRouter>
-                    <Header />
-                </MemoryRouter>
-            );
-            const logo = screen.getByTestId('logo');
-            expect(logo.className).toBe('header__logo');
-        });
-    });
+	describe('when I am on a Home page', () => {
+		test('Then,there should be on the page', () => {
+			render(
+				<MemoryRouter>
+					<Header />
+				</MemoryRouter>
+			);
+			const logo = screen.getByTestId('logo');
+			expect(logo.className).toBe('header__logo');
+		});
+	});
 });
