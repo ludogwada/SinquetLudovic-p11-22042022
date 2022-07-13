9515969029343b3ac92acdb85c6797dd7ec7b8fa
ludogwada <83616314+ludogwada@users.noreply.github.com>
2022-07-13 10:47:13 -0400

    fix bug key

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,31 +1,29 @@
-import Banner from '../components/Banner/banner'
-import image from '../assets/imageAPropos.png'
-import APropos from '../data/A-propos.json'
-import Collapse from '../components/Collapse/collapse'
-import '../index.css'
+import Banner from '../components/Banner/banner';
+import image from '../assets/imageAPropos.png';
+import APropos from '../data/A-propos.json';
+import Collapse from '../components/Collapse/collapse';
+import '../index.css';
 
-
-
-function About(){
-    return(
-        <main>
-            <article className='aboutBanner'>
-                <Banner
-                    image={image} />
-            </article>
-            <article className='about'>
-                {APropos.map((collapse, index) => {
-                    return(
-                        <article className='about__collapse'>
-                            <Collapse 
-                                key={index}
-                                title={collapse.title}
-                                description= {collapse.description}/>
-                        </article>
-                    )
-                    })}
-            </article>    
-        </main>)
+function About() {
+	return (
+		<main>
+			<article className='aboutBanner'>
+				<Banner image={image} />
+			</article>
+			<article className='about'>
+				{APropos.map((collapse, index) => {
+					return (
+						<article className='about__collapse' key={index}>
+							<Collapse
+								title={collapse.title}
+								description={collapse.description}
+							/>
+						</article>
+					);
+				})}
+			</article>
+		</main>
+	);
 }
 
-export default About
+export default About;
--- src/pages/ficheLogement.jsx
+++ src/pages/ficheLogement.jsx
@@ -1,66 +1,62 @@
-import { useState } from "react";
-import { Navigate, useParams } from "react-router-dom";
-import Collapse from "../components/Collapse/collapse";
-import Host from "../components/Host/host";
-import Rating from "../components/rating/rating";
-import Slider from "../components/slider/slider";
-import Tag from "../components/Tag/tag";
-import ListHebergement from '../data/Liste-hebergement.json'
- 
-
-
+import { useState } from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+import Collapse from '../components/Collapse/collapse';
+import Host from '../components/Host/host';
+import Rating from '../components/rating/Rating';
+import Slider from '../components/slider/slider';
+import Tag from '../components/Tag/tag';
+import ListHebergement from '../data/Liste-hebergement.json';
 
 function Fiche() {
+	const { hebergementId } = useParams();
+	const [hebergementData] = useState(ListHebergement);
 
-    const { hebergementId } = useParams();
-    const [hebergementData] = useState(ListHebergement)
-    
-    let hebergement = hebergementData.find( hebergement => hebergement.id === hebergementId)
+	let hebergement = hebergementData.find(
+		(hebergement) => hebergement.id === hebergementId
+	);
 
-    if(!hebergement){
-        return <Navigate to='*' />
-    }
-    
-    return(
-        <main>
-            <section className="cardLocation">
-                <Slider
-                    pictures={hebergement.pictures}
-                    title={hebergement.title} />
-                <section className="cardLocation__titleHost">
-                    <section>
-                        <h2>{hebergement.title}</h2>
-                        <h4>{hebergement.location}</h4>
-                    </section>
-                    <Host 
-                        name={hebergement.host.name}
-                        picture={hebergement.host.picture}/>
-                </section>
-                
-                <section className="cardLocation__tagRating">
-                    <Tag
-                        tags={hebergement.tags} />
-                    <Rating
-                        rating={hebergement.rating} />
-                </section>
-                <section className="cardLocation__collapse">
-                    <section className="cardLocation__collapse__description">
-                        <Collapse
-                            title= 'Description'
-                            description={hebergement.description} />
-                    </section>
-                    <section className="cardLocation__collapse__equipement">
-                        <Collapse
-                            title= 'Equipement'
-                            description={hebergement.equipments.map((equipment, index) =>
-                            (<li key={index}>{equipment}</li>))} />
-                    </section>
-                </section>
-                   
+	if (!hebergement) {
+		return <Navigate to='*' />;
+	}
 
-            </section>
-        </main>
-    )
+	return (
+		<main>
+			<section className='cardLocation'>
+				<Slider pictures={hebergement.pictures} title={hebergement.title} />
+				<section className='cardLocation__titleHost'>
+					<section>
+						<h2>{hebergement.title}</h2>
+						<h4>{hebergement.location}</h4>
+					</section>
+					<Host
+						name={hebergement.host.name}
+						picture={hebergement.host.picture}
+					/>
+				</section>
+
+				<section className='cardLocation__tagRating'>
+					<Tag tags={hebergement.tags} />
+					<Rating rating={hebergement.rating} />
+				</section>
+				<section className='cardLocation__collapse'>
+					<section className='cardLocation__collapse__description'>
+						<Collapse
+							title='Description'
+							description={hebergement.description}
+						/>
+					</section>
+					<section className='cardLocation__collapse__equipement'>
+						<Collapse
+							title='Equipement'
+							description={hebergement.equipments.map((equipment, index) => (
+								<li key={index}>{equipment}</li>
+							))}
+						/>
+					</section>
+				</section>
+			</section>
+		</main>
+	);
 }
 
-export default Fiche
+export default Fiche;
